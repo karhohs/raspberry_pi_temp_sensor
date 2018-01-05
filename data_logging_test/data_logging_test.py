aa870f68d8a25f4750a97acf5aebfd08e60f8259
karhohs <gandalfisarockstar@gmail.com>
2018-01-05 02:04:25 -0500

    update demo for csv files

--- data_logging_test/data_logging_test.py
+++ data_logging_test/data_logging_test.py
@@ -9,7 +9,17 @@ def data_logging_demo():
         for i in range(5):
             now = datetime.now()
             print(str(now))
-            f.write(str(now))
+            f.write(', '.join([str(now)])+"\n")
+            time.sleep(5)
+
+def data_logging_demo_csv():
+# The user of the Docker container is root. The root directory is `/root`.
+# Be sure to mount a volume to /root from the host system.
+    with open("/root/data/temperature_log.csv", "a") as f:
+        for i in range(5):
+            now = datetime.now()
+            print(str(now))
+            f.write(', '.join([str(now)])+"\n")
             time.sleep(5)
 
 if __name__ == "__main__":
