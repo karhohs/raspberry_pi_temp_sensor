2c987ddd9f45b1c25472ef1fac33089c3f44711b
karhohs <gandalfisarockstar@gmail.com>
2018-01-04 20:42:05 -0500

    open log file in mode 'a'

--- data_logging_test/data_logging_test.py
+++ data_logging_test/data_logging_test.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def data_logging_demo():
 # The user of the Docker container is root. The root directory is `/root`.
 # Be sure to mount a volume to /root from the host system.
-    with open("/root/data/temperature_log.csv") as f:
+    with open("/root/data/temperature_log.csv", "a") as f:
         for i in range(5):
             now = datetime.now()
             print(str(now))
--- temperature_sensor/temperature_sensor.py
+++ temperature_sensor/temperature_sensor.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 
 def temperature_sensor():
-    with open("/root/temperature_log.csv") as f:
+    with open("/root/temperature_log.csv", "a") as f:
         for i in range(5):
             now = datetime.now()
             f.write(str(now))
